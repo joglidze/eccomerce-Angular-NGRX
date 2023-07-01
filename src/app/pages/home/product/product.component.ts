a566741f393c709308f09218d3b81a7a4b5572c0
luca <lucajoglidze@gmail.com>
2023-07-01 18:21:30 +0400

    home page responsivness

--- src/app/pages/home/product/product.component.ts
+++ src/app/pages/home/product/product.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
 import { Subject } from 'rxjs';
 import { register } from 'swiper/element/bundle';
 import { productsState } from '../store/home.select';
+import { Swiper, SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-product',
@@ -12,13 +13,18 @@ import { productsState } from '../store/home.select';
 })
 export class ProductComponent implements OnInit, OnDestroy {
   productss: any;
+  screenWidth: any;
+  slideCount = 2;
   sub$ = new Subject();
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.selectProducts()
+    this.screenWidth = window.innerWidth;
+    console.log(this.screenWidth);
+    this.selectProducts();
     register();
+  
   }
 
   selectProducts() {
@@ -27,8 +33,15 @@ export class ProductComponent implements OnInit, OnDestroy {
     });
   }
 
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.screenWidth = window.innerWidth;
+    console.log(this.screenWidth);
+  }
+
   ngOnDestroy(): void {
     this.sub$.next(null);
     this.sub$.complete();
   }
+  
 }
